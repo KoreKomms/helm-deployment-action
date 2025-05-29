a470061e8ebd14e070980967ec05c218ea2fbb76
Sudipto Sarkar <sudiptosarkar@visioplanet.org>
2025-05-29 19:56:40 +0530

    Fixes for descriptions, cleanup and kube context
    * Fixed Action Description to be more precise
    * Fixed helm command by adding --kube-context for context switching based on environment
    * Fixed helmChartVersion append
    * Updated README to be more descriptive
    * Added kubeConfig file cleanup step
    * Added support for environment variables
    * Fixed context path setting
    * Decouple helm execution from the action yaml because of env variable leaks in run logs

---
+++ src/helm.env.var.dto.ts
@@ -0,0 +1,4 @@
+export interface HelmEnvVarDto {
+  name: string;
+  value: string;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,24 @@
+import { execSync } from 'child_process';
 import { GithubDto } from './github.value.dto';
+import { HelmEnvVarDto } from './helm.env.var.dto';
 import { HelmVariableDto } from './helm.variable.dto';
 
-const dryRun = process.env.DRY_RUN;
+const dryRun = process.env.DRY_RUN === 'true';
 const name = process.env.NAME;
 const helmChartUrl = process.env.HELM_CHART_URL;
+const helmChartVersion = process.env.HELM_CHART_VERSION;
 const namespace = process.env.NAMESPACE;
-const secrets: { [key: string]: string } = JSON.parse(process.env.GITHUB_SECRETS ?? '{}');
-const variables: { [key: string]: string } = JSON.parse(process.env.GITHUB_VARIABLES ?? '{}');
+const secrets: { [key: string]: string } = JSON.parse(process.env.GITHUB_SECRETS || '{}');
+const variables: { [key: string]: string } = JSON.parse(process.env.GITHUB_VARIABLES || '{}');
+const environmentVariables: { [key: string]: string } = JSON.parse(process.env.ENVIRONMENT_VARIABLES || '{}');
 const prefix = process.env.GITHUB_SECRET_VARIABLE_PREFIX;
 const environment = process.env.DEPLOYMENT_ENVIRONMENT;
 const helmConfigMapVariableName = process.env.HELM_CONFIG_MAP_VARIABLE_NAME;
 const helmSecretVariableName = process.env.HELM_SECRET_VARIABLE_NAME;
+const helmEnvVarVariableName = process.env.HELM_ENV_VAR_VARIABLE_NAME;
 const tag = process.env.TAG;
 
-let helmDeploymentCommand = 'helm --kubeconfig .kubeConfig upgrade \\\n' + '  --install \\\n' + '  --create-namespace \\\n' + `  --namespace ${namespace} \\\n` + `  --set image.tag=${tag} \\\n`;
+let helmDeploymentCommand = `helm --kubeconfig .kubeConfig --kube-context ${environment} upgrade ${name} ${dryRun ? '\\\n  ' : ''}--install ${dryRun ? '\\\n  ' : ''}--create-namespace ${dryRun ? '\\\n  ' : ''}--namespace ${namespace} ${dryRun ? '\\\n  ' : ''}--set image.tag=${tag} ${dryRun ? '\\\n  ' : ''}`;
 
 if (dryRun) {
   console.log('Executing Dry Run...');
@@ -21,6 +26,7 @@ if (dryRun) {
 
 const helmSecrets: Array<HelmVariableDto> = [];
 const helmConfigMaps: Array<HelmVariableDto> = [];
+const helmEnvVars: Array<HelmEnvVarDto> = [];
 
 if (!helmChartUrl) {
   throw new Error('Misconfigured action. Helm chart URL is missing.');
@@ -34,7 +40,7 @@ Object.keys(secrets).forEach((secretName) => {
         if (dryRun) {
           console.log(`Configuring secret with key: ${secretName}`);
         }
-        helmSecrets.push({ key: varValue.key, value: varValue.value });
+        helmSecrets.push({ key: varValue.key, value: varValue.value.replace(',', '\\,') });
       }
     } catch (e: unknown) {
       console.error(`Misconfigured Github Secret: ${secretName}. Please correct and re-run.`);
@@ -51,7 +57,7 @@ Object.keys(variables).forEach((variableName) => {
         if (dryRun) {
           console.log(`Configuring Variable with key: ${variableName}`);
         }
-        helmConfigMaps.push({ key: varValue.key, value: varValue.value });
+        helmConfigMaps.push({ key: varValue.key, value: varValue.value.replace(',', '\\,') });
       }
     } catch (e: unknown) {
       console.error(`Misconfigured Github Variable: ${variableName}. Please correct and re-run.`);
@@ -60,20 +66,31 @@ Object.keys(variables).forEach((variableName) => {
   }
 });
 
+Object.keys(environmentVariables).forEach((environmentVariableName) => helmEnvVars.push({ name: environmentVariableName, value: environmentVariables[environmentVariableName].replace(',', '\\,') }));
+
 helmSecrets.forEach((dto, idx) => {
-  helmDeploymentCommand = helmDeploymentCommand.concat(`  --set ${helmSecretVariableName}[${idx}].key='${dto.key}' \\\n`);
-  helmDeploymentCommand = helmDeploymentCommand.concat(`  --set ${helmSecretVariableName}[${idx}].value='${dryRun ? '<REDACTED>' : dto.value}' \\\n`);
+  helmDeploymentCommand = helmDeploymentCommand.concat(`--set ${helmSecretVariableName}[${idx}].key='${dto.key}' ${dryRun ? '\\\n  ' : ''}`);
+  helmDeploymentCommand = helmDeploymentCommand.concat(`--set ${helmSecretVariableName}[${idx}].value='${dryRun ? '<REDACTED>' : dto.value}' ${dryRun ? '\\\n  ' : ''}`);
 });
 
 helmConfigMaps.forEach((dto, idx) => {
-  helmDeploymentCommand = helmDeploymentCommand.concat(`  --set ${helmConfigMapVariableName}[${idx}].key='${dto.key}' \\\n`);
-  helmDeploymentCommand = helmDeploymentCommand.concat(`  --set ${helmConfigMapVariableName}[${idx}].value='${dryRun ? '<REDACTED>' : dto.value}' \\\n`);
+  helmDeploymentCommand = helmDeploymentCommand.concat(`--set ${helmConfigMapVariableName}[${idx}].key='${dto.key}' ${dryRun ? '\\\n  ' : ''}`);
+  helmDeploymentCommand = helmDeploymentCommand.concat(`--set ${helmConfigMapVariableName}[${idx}].value='${dryRun ? '<REDACTED>' : dto.value}' ${dryRun ? '\\\n  ' : ''}`);
 });
 
-helmDeploymentCommand = helmDeploymentCommand.concat('  '.concat(helmChartUrl));
+helmEnvVars.forEach((dto, idx) => {
+  helmDeploymentCommand = helmDeploymentCommand.concat(`--set ${helmEnvVarVariableName}[${idx}].name='${dto.name}' ${dryRun ? '\\\n  ' : ''}`);
+  helmDeploymentCommand = helmDeploymentCommand.concat(`--set ${helmEnvVarVariableName}[${idx}].value='${dto.value}' ${dryRun ? '\\\n  ' : ''}`);
+});
 
-if (dryRun) {
-  console.log(`This is a Dry Run. Install command to be run - ${helmDeploymentCommand}`);
+if (helmChartVersion) {
+  helmDeploymentCommand = helmDeploymentCommand.concat(`--version ${helmChartVersion} ${dryRun ? '\\\n  ' : ''}`);
 }
 
-console.log(helmDeploymentCommand);
+helmDeploymentCommand = helmDeploymentCommand.concat(helmChartUrl);
+
+if (dryRun) {
+  console.log(`This is a Dry Run. Install command to be run: ${helmDeploymentCommand}`);
+} else {
+  execSync(helmDeploymentCommand);
+}
